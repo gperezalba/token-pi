19e92966ad6158f0284023621511c8865b378451
Guillermo Pérez Alba <gperezalba94@gmail.com>
2019-04-05 12:09:38 +0200

    Add multiple pages

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,17 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>Token-Pi</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+      <v-btn to="/" class="mr-2">Home</v-btn>
+      <v-btn to="/contact" class="mr-2">Contact</v-btn>
+      <v-btn to="/about" class="mr-2">About</v-btn>
+      <v-btn to="/members" class="mr-2">Members</v-btn>
     </v-toolbar>
 
     <v-content>
-      <HelloWorld/>
+      <router-view></router-view>
     </v-content>
   </v-app>
 </template>
